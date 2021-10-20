184dcd6c5668deebbba34884b971bbf51c299c94
Hua Luo <luohuazju@gmail.com>
2021-10-19 21:53:56 -0500

    init the project

---
+++ src/main/java/com/sillycat/zeppelinapi/ExecutorApplication.java
@@ -0,0 +1,16 @@
+package com.sillycat.zeppelinapi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExecutorApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(ExecutorApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("start the application-------------");
+
+		logger.info("application finished--------------");
+	}
+
+}
